bd265fbd72227bb369b5d5931ba35fbd0133bdb2
Pawel Niewiadomski <11110000b@gmail.com>
2013-10-30 20:52:20 +0100

    working on angular integration

---
+++ src/main/resources/js/jira-openid-configuration.js
@@ -0,0 +1,12 @@
+var configuration = angular.module("openid.configuration", [])
+    .constant('contextPath', contextPath)
+    .config(['$interpolateProvider', function($interpolateProvider) {
+        $interpolateProvider.startSymbol("[[");
+        $interpolateProvider.endSymbol("]]");
+    }]);
+
+var ConfigurationCtrl = ['$scope', '$http', 'contextPath', function($scope, $http, contextPath) {
+    $http.get(contextPath + "/rest/jira-openid-authentication/1.0/openIdProviders").success(function(data) {
+        $scope.providers = data;
+    });
+}];
