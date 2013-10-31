14118d8700ea9e8c7626c3c84b8529ab732ce123
Pawel Niewiadomski <11110000b@gmail.com>
2013-10-31 06:53:40 +0100

    showing error when unable to load providers

--- src/main/resources/js/jira-openid-configuration.js
+++ src/main/resources/js/jira-openid-configuration.js
@@ -8,5 +8,9 @@ var configuration = angular.module("openid.configuration", [])
 var ConfigurationCtrl = ['$scope', '$http', 'contextPath', function($scope, $http, contextPath) {
     $http.get(contextPath + "/rest/jira-openid-authentication/1.0/openIdProviders").success(function(data) {
         $scope.providers = data;
+        $scope.loaded = true;
+        $scope.error = false;
+    }).error(function(data) {
+        $scope.error = true;
     });
 }];
