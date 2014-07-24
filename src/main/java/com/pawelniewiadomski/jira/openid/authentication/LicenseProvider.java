595d041668166328b133d2354528859260c83dcf
Paweł Niewiadomski <pniewiadomski@atlassian.com>
2014-07-24 08:37:56 +0200

    Revert "removed usage of licensing api"
    This reverts commit a3010dfa5953785310e2d29268fa0bb12e5da495.

--- src/main/java/com/pawelniewiadomski/jira/openid/authentication/LicenseProvider.java
+++ src/main/java/com/pawelniewiadomski/jira/openid/authentication/LicenseProvider.java
@@ -1,12 +1,34 @@
 package com.pawelniewiadomski.jira.openid.authentication;
 
+import com.atlassian.upm.api.license.entity.PluginLicense;
+import com.atlassian.upm.license.storage.lib.PluginLicenseStoragePluginUnresolvedException;
+import com.atlassian.upm.license.storage.lib.ThirdPartyPluginLicenseStorageManager;
+import com.google.common.collect.Iterables;
+
 /**
  *
  */
 public class LicenseProvider {
 
     public boolean isValidLicense() {
-        return true;
+        try {
+            try {
+                final ThirdPartyPluginLicenseStorageManager licenseManager = Iterables.<ThirdPartyPluginLicenseStorageManager>getFirst(
+                        SpringContext.getApplicationContext().getBeansOfType(ThirdPartyPluginLicenseStorageManager.class).values(), null);
+                if (licenseManager.getLicense().isDefined())
+                {
+                    for (PluginLicense pluginLicense : licenseManager.getLicense())
+                    {
+                        return !pluginLicense.getError().isDefined();
+                    }
+                }
+            } catch (PluginLicenseStoragePluginUnresolvedException e) {
+                return false;
+            }
+        } catch (ClassCastException e) {
+            return false;
+        }
+        return false;
     }
 
 }
