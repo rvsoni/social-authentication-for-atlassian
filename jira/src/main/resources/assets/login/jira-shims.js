631c33253d220a2a80199e6d57549751eda2a220
Pawel Niewiadomski <11110000b@gmail.com>
2015-09-13 19:45:38 +0200

    Revert "copying stuff to common, jira, confluence modules"
    This reverts commit ded1d6479e8af20e681af908a151251bc71b3480.

--- jira/src/main/resources/assets/login/jira-shims.js
+++
@@ -1,3 +0,0 @@
-define('ajs', [], function() {
-    return AJS;
-});
--- jira/src/test/java/it/confluence/pageobjects/ConfluenceLoginPage.java
+++
@@ -1,8 +0,0 @@
-package it.confluence.pageobjects;
-
-public class ConfluenceLoginPage extends it.jira.pageobjects.JiraLoginPage {
-    @Override
-    public String getUrl() {
-        return "/login.action";
-    }
-}
