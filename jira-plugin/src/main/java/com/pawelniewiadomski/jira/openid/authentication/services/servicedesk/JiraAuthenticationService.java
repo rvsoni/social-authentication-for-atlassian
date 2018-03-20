0256841e689ee30a809b7a8fe0d95ae93e157a5f
Pawel Niewiadomski <11110000b@gmail.com>
2018-03-20 18:15:01 +0100

    dropped jira 6.x bridges as they won't be needed switched to use UserManager iso UserUtil in JiraAuthenticationService.java changed jira.version to 7.2.13 cleaned up poms from duplicate versions

--- jira-plugin/src/main/java/com/pawelniewiadomski/jira/openid/authentication/services/servicedesk/JiraAuthenticationService.java
+++ jira-plugin/src/main/java/com/pawelniewiadomski/jira/openid/authentication/services/servicedesk/JiraAuthenticationService.java
@@ -5,19 +5,17 @@ import com.atlassian.crowd.search.query.entity.UserQuery;
 import com.atlassian.crowd.search.query.entity.restriction.MatchMode;
 import com.atlassian.crowd.search.query.entity.restriction.TermRestriction;
 import com.atlassian.crowd.search.query.entity.restriction.constants.UserTermKeys;
-import com.atlassian.jira.compatibility.bridge.user.UserUtilBridge;
-import com.atlassian.jira.compatibility.factory.user.UserUtilBridgeFactory;
-import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.security.login.LoginManager;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.ApplicationUsers;
+import com.atlassian.jira.user.UserDetails;
+import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.seraph.auth.DefaultAuthenticator;
 import com.atlassian.seraph.service.rememberme.RememberMeService;
 import com.google.common.collect.Iterables;
 import com.pawelniewiadomski.jira.openid.authentication.activeobjects.OpenIdProvider;
 import com.pawelniewiadomski.jira.openid.authentication.services.*;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -35,7 +33,7 @@ import static org.apache.commons.lang.StringUtils.*;
 @Slf4j
 @Component
 public class JiraAuthenticationService implements AuthenticationService {
-    @Autowired protected UserUtilBridgeFactory userUtilBridgeFactory;
+    @Autowired protected UserManager userManager;
 
     @Autowired protected CrowdService crowdService;
 
@@ -61,25 +59,30 @@ public class JiraAuthenticationService implements AuthenticationService {
             }
         }
 
-        com.atlassian.crowd.embedded.api.User user = (com.atlassian.crowd.embedded.api.User) Iterables.getFirst(crowdService.search(new UserQuery(
+        final com.atlassian.crowd.embedded.api.User user = (com.atlassian.crowd.embedded.api.User) Iterables.getFirst(crowdService.search(new UserQuery(
                 com.atlassian.crowd.embedded.api.User.class, new TermRestriction(UserTermKeys.EMAIL, MatchMode.EXACTLY_MATCHES,
                 stripToEmpty(email).toLowerCase()), 0, 1)), null);
 
+        final ApplicationUser appUser;
         if (user == null && !externalUserManagementService.isExternalUserManagement() && globalSettings.isCreatingUsers()) {
             try {
-                final UserUtilBridge userUtil = ((UserUtilBridge) userUtilBridgeFactory.getObject());
-                user = ApplicationUsers.toDirectoryUser(userUtil.createUserNoNotification(lowerCase(replaceChars(identity, " '()", "")), randomUUID().toString(),
-                        email, identity));
+                final String userName = lowerCase(replaceChars(identity, " '()", ""));
+
+                appUser = userManager.createUser(
+                        new UserDetails(userName, identity)
+                            .withPassword(randomUUID().toString())
+                            .withEmail(email)
+                );
             } catch (Exception e) {
                 log.error(String.format("Cannot create an account for %s %s", identity, email), e);
                 templateHelper.render(request, response, "OpenId.Templates.error");
                 return;
             }
+        } else {
+            appUser = ApplicationUsers.from(user);
         }
 
         if (user != null) {
-            final ApplicationUser appUser = ApplicationUsers.from(user);
-
             final HttpSession httpSession = request.getSession();
             httpSession.setAttribute(DefaultAuthenticator.LOGGED_IN_KEY, appUser);
             httpSession.setAttribute(DefaultAuthenticator.LOGGED_OUT_KEY, null);
